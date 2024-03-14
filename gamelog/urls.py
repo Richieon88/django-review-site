229b44082d3cf9c09733e26079518f64a177759f
Richieon88 <richieoneill88@gmail.com>
2024-03-14 21:50:45 +0000

    Add 'game_list' view for 'games' app

--- gamelog/urls.py
+++ gamelog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from games.views import game_list
 
 urlpatterns = [
+    path('games/', game_list, name='games'),
     path('admin/', admin.site.urls),
 ]
