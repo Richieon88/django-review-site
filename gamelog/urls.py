70cc0bf5c5e5536dbea911d2acb100ad141eb0de
Richieon88 <richieoneill88@gmail.com>
2024-03-20 23:28:54 +0000

    add templates and views for 'reviews' app

--- gamelog/urls.py
+++ gamelog/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('games/', include('games.urls')),
+    path('reviews/', include('reviews.urls')),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Review
+
 
 # Create your views here.
+def review_list(request):
+    reviews = Review.objects.all()
+    return render(request, 'reviews/review_list.html', {'reviews': reviews})
+
+def review_detail(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    return render(request, 'reviews/review_detail.html', {'review': review})
+
+def submit_review(request):
+    if request.method == 'POST':
+        return render(request, 'reviews/review_list.html')
+    return render(request, 'reviews/submit_review.html')
