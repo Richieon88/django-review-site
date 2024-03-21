d068db55ce085ee1082b59c9b45ad171fcda0c1b
Richieon88 <richieoneill88@gmail.com>
2024-03-21 23:10:57 +0000

    add index.html and 'homepage' view

--- games/urls.py
+++ games/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.game_list, name='game_list'),
     path('<int:pk>/', views.game_detail, name='game_detail'),
+    path('', views.homepage, name='homepage'),
 ]
--- games/views.py
+++ games/views.py
@@ -9,3 +9,12 @@ def game_list(request):
 def game_detail(request, pk):
     game = Game.objects.get(pk=pk)
     return render(request, 'games/game_detail.html', {'game': game})
+
+def homepage(request):
+    latest_games = Game.objects.order_by('-release_date')[:3]  # Get latest 3 games
+    top_rated_games = Game.objects.order_by('-rating')[:3]    # Get top rated 3 games
+    context = {
+        'latest_games': latest_games,
+        'top_rated_games': top_rated_games,
+    }
+    return render(request, 'index.html', context)
