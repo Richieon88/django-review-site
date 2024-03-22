b8480b7f52ea1183adc0392a7e2dad4534933b7c
Richieon88 <richieoneill88@gmail.com>
2024-03-22 23:04:37 +0000

    Add templates and urls for user authentication

--- gamelog/urls.py
+++ gamelog/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('games/', include('games.urls')),
     path('reviews/', include('reviews.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
