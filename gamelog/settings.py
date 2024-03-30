71f28141fbf3b4b50137c8e81d732b95edf4d257
Richieon88 <richieoneill88@gmail.com>
2024-03-30 05:29:23 +0000

    Add truncation to reviews on homepage

--- gamelog/settings.py
+++ gamelog/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-richieon88-django-review-0ezoy77x6u.us1.codeanyapp.com',
 '.herokuapp.com']
